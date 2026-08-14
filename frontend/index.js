5d1c3ec934bbd0860c649f5d2fdedc3650a38b0d
Annam Chaithanya <annamchaithanya07@gmail.com>
2026-08-14 10:47:44 +0530

    random user

--- frontend/index.js
+++ frontend/index.js
@@ -24,3 +24,26 @@ function toggleUser() {
     userGender.innerHTML = users[curId].gender;
     userImage.src = users[curId].image;
 }
+
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        var userNAme = document.getElementById("user-name");
+        var userGender = document.getElementById("user-gender");
+        var userImage = document.getElementById("user-image");
+
+        var newUserName = data.results[0].name.first + " " + data.results[0].name.last;
+        var newUserGender = data.results[0].gender;
+        var newUserImage = data.results[0].picture.large;
+
+        userNAme.innerHTML = newUserName;
+        userGender.innerHTML = newUserGender;
+        userImage.src = newUserImage;
+    })
+
+    .catch(function(err){
+        console.log("Error occured : " + err);
+})}
